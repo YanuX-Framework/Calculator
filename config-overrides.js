2bc783902ef0e15db5bb1fbfdb5fc3a09fefd75f
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-10-14 21:07:48 +0100

    - Package Updates and Minor tweaks to the Webpack Config Override

--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,13 @@ const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
 
 module.exports = {
     webpack: function (config, env) {
+        //TODO: Check if this is still needed.
         config.devtool = 'source-map'
         config.module.rules.push({ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' })
         
-        config.plugins.push(new FilterWarningsPlugin({ exclude: /Failed to parse source map/ }))
-        config.stats = { warningsFilter: [/Failed to parse source map/] }
+        //TODO: Remove since it seems to no longer be needed.
+        //config.plugins.push(new FilterWarningsPlugin({ exclude: /Failed to parse source map/ }))
+        //config.stats = { warningsFilter: [/Failed to parse source map/] }
         
         return config
     }
